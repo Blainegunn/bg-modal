b3095655de3ebbca8de77a3fa42ee1974441fee7
Blaine Gunn <blainergunn@Blaines-MacBook-Pro.local>
2015-05-04 16:46:16 -0600

    second run

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
-/**
- * Escape special characters in the given string of html.
- *
- * @param  {String} html
- * @return {String}
- */
-module.exports = {
-  escape: function(html) {
-    return String(html)
-      .replace(/&/g, '&amp;')
-      .replace(/"/g, '&quot;')
-      .replace(/'/g, '&#39;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;');
-  },
-
-  /**
-   * Unescape special characters in the given string of html.
-   *
-   * @param  {String} html
-   * @return {String}
-   */
-  unescape: function(html) {
-    return String(html)
-      .replace(/&amp;/g, '&')
-      .replace(/&quot;/g, '"')
-      .replace(/&#39;/g, ''')
-      .replace(/&lt;/g, '<')
-      .replace(/&gt;/g, '>');
+ module.exports = {
+  blue: function(){
+    console.log('I never knew');
+    return 'Leaving for the day. We miss you.'
   }
-};
+ }
